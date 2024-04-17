e2dc3e89d2f63e971c210021841e2752896b7d66
Katie Bridson <kbridson@memphis.edu>
2024-04-17 15:07:41 -0500

    Index and show pages working, no link

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,11 @@
+class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+    render :index
+  end
+
+  def show
+    @car = Car.find(params[:id])
+    render :show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'cars', to: 'cars#index', as: 'cars'
+  get 'cars/:id', to: 'cars#show', as: 'car'
 end
